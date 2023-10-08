343720794820a0a582ac2ed7fd426496862b4f6b
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2023-10-08 15:37:44 +0300

    Update script.js

--- laba_5/script.js
+++ laba_5/script.js
@@ -1,29 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var stoimost = document.getElementById("stoimost");
     stoimost.addEventListener("click", calc);
-  });
+});
 
-  function validate() {
+function validate() {
     var count = document.getElementById("count").value;
     var o = /^[\d]$/;
     var valid = o.test(count);
 
-    if(valid) output = "Правильный ввод";
-    else output = "Некорректный ввод";
-    document.getElementById("message").innerHTML = output;
+    if (valid) {
+        window.output = "Правильный ввод";
+    } else {
+        window.output = "Некорректный ввод";
+    }
+    document.getElementById("message").innerHTML = window.output;
     return valid;
-  }
+}
 
-  function calc() {
+function calc() {
     var name = document.getElementById("select1");
     var count = document.getElementById("count");
     var result = document.getElementById("result");
+    var price = 0;
 
-    if(validate()){
-        var price = 0;
+    if (validate()) {
         price += parseInt(name.options[name.selectedIndex].value);
         price = parseInt(count.value) * price;
         result.innerHTML = price;
+    } else {
+        window.alert("GG");
     }
-    else alert("GG");
-  }
+}
