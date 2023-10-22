19ce64f78de29cfdd7e4c7ebe9dad413b5438fba
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2023-10-22 15:15:11 +0300

    Update script.js

--- laba_7/script.js
+++ laba_7/script.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
     $('.slider').slick({
         arrows: true,
+        infinity: true,
         dots: true, //точки
         slidesToShow: 3, // сколько показывается на экране
         slidesToScroll: 1, //сколько пролистывается
-
         responsive:[
             {
-				breakpoint: 962,
-				settings: {
-					slidesToShow: 2,
-				}
-			},
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
             {
                 breakpoint: 480, //при таком разрешении выводится только 1 слайдер
                 settings: {
                     slidesToShow: 1,
                 }
             }
-        ],
+        ]
     });
 });
