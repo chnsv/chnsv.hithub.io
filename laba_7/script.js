40ab6186b5b7106e34aeab587d1dc36afaeb5376
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2023-10-22 16:30:07 +0300

    Update script.js

--- laba_7/script.js
+++ laba_7/script.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
         dots: true, //точки
         slidesToShow: 3, // сколько показывается на экране
         slidesToScroll: 1, //сколько пролистывается
+        prevArrow: "<img src='sl-arrow_l.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='sl-arrow_r.svg' class='next' alt='2'>",
         responsive:[
             {
                 breakpoint: 600,
