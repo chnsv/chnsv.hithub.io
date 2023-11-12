b72df1b69f9a83b42f0509f0212921746c3986cd
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2023-11-12 15:56:26 +0300

    Update script.js

--- laba_8/script.js
+++ laba_8/script.js
@@ -4,13 +4,12 @@ const popup = document.querySelector(".popup");
 button.addEventListener("click",()=>{
     form.classList.add("open");
     popup.classList.add("popup_open");
+    history.pushState(null, null, 'page1.html');
+    history.forward();
 });
 
-localHistory.pushState(null, null, 'page1.html');
-localHistory.forward();
 
-history.back();
-localHistory.replaceState({opened:false},"",window.location.href);
+history.replaceState({opened:false},"",window.location.href);
 $(window).on("popstate",()=>{
   Down();
 })
