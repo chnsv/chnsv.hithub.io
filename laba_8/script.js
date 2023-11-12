87851a6a8cddf52d8d3a69f7953a28fe64dd8f73
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2023-11-12 15:01:04 +0300

    Update script.js

--- laba_8/script.js
+++ laba_8/script.js
@@ -6,7 +6,22 @@ button.addEventListener("click",()=>{
     popup.classList.add("popup_open");
 });
 
+//чтоб форма самостоятельно не отправлялась
+function handleFormSubmit(event) {
+  event.preventDefault()
+  console.log('Отправка!')
+}
+const applicantForm = document.getElementById('myForm')
+applicantForm.addEventListener('submit', handleFormSubmit)
+
+
 let Obj = {
   form: "EnterForm",
 };
 history.pushState(Obj, "page 2", "form.html");
+
+$(window).on("popstate", ()=> {
+  Down();
+});
+
+history.back();
