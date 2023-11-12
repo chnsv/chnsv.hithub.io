e2f53224bb55f76d598d63b484b5664811d3c5c8
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2023-11-12 12:55:48 +0300

    Update script.js

--- laba_8/script.js
+++ laba_8/script.js
@@ -5,3 +5,8 @@ button.addEventListener("click",()=>{
     form.classList.add("open");
     popup.classList.add("popup_open");
 });
+
+let Obj = {
+  form: "EnterForm",
+};
+history.pushState(Obj, "page 2", "form.html");
