e0c74350750cddb679c71cdfe827aa6c66136d1f
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2023-12-18 18:26:36 +0300

    Add files via upload

---
+++ проект/src/App.js
@@ -0,0 +1,30 @@
+import './App.scss';
+import HeaderSection from './components/HeaderSection';
+import ServiceSection from './components/ServiceSection';
+import SupportSection from './components/SupportSection';
+import ExpertiseSection from './components/ExpertiseSection';
+import PlansSection from './components/PlansSection';
+import CasesSection from './components/CasesSection';
+import TeamsSection from './components/TeamsSection';
+import SliderSection from './components/SliderSection';
+import PartnersSection from './components/PartnersSection';
+import FormSection from './components/FormSection';
+
+function App() {
+  return (
+   <>
+    <HeaderSection />
+    <ServiceSection />
+    <SupportSection />
+    <ExpertiseSection />
+    <PlansSection />
+    <CasesSection />
+    <TeamsSection />
+    <SliderSection />
+    <PartnersSection />
+    <FormSection />
+   </>
+  );
+}
+
+export default App;
