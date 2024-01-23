b9f2081592dceb18fd3e79e8e2ea980bafd45daa
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2024-01-23 21:45:25 +0300

    Update script.js

--- laba_6/script.js
+++ laba_6/script.js
@@ -1,135 +1,33 @@
-var curr;
-
-window.addEventListener("DOMContentLoaded", function (event) {
-    var s = document.getElementById("myselect");
-    var ch = document.querySelectorAll(".checkboxes input[type=checkbox]");
-    curr = 0;
-    s.addEventListener("change", function (event) {
-        var select = event.target;
-        var vibor = document.getElementById("select1");
-        var check = document.getElementById("checkboxes");
-        var radios = document.getElementById("myradios");
-        var count = document.getElementById("count");
-        if (select.value === "0") {
-            vibor.style.display = "none";
-            check.style.display = "none";
-            radios.style.display = "none";
-            count.style.display = "none";
-        }
-        if (select.value === "1") {
-            vibor.style.display = "none";
-            check.style.display = "none";
-            radios.style.display = "block";
-            count.style.display = "block";
-        }
-        if (select.value === "2") {
-            vibor.style.display = "block";
-            check.style.display = "none";
-            radios.style.display = "block";
-            count.style.display = "block";
-        }
-        if (select.value === "3") {
-            vibor.style.display = "none";
-            check.style.display = "block";
-            radios.style.display = "block";
-            count.style.display = "block";
-        }
-        curr = parseInt(select.value);
-    });
-    ch.forEach(function (checkbox) {
-        checkbox.addEventListener("change", function (event) {
-            ch = event.target;
-        });
-    });
-});
-function validate() {
-    var count = document.getElementById("count").value;
-    var o = /^\d{1,}$/;
-    var valid = o.test(count);
-
-    if (valid) {
-        window.output = "Правильный ввод";
-    } else {
-        window.output = "Некорректный ввод";
-    }
-    document.getElementById("message").innerHTML = window.output;
-    return valid;
-}
-
-function calc() {
-    var result = document.getElementById("result");
-    var res;
-    if (validate()) {
-        if (curr === 1) {
-            res = calcsertif();
-            result.innerHTML = res;
-        }
-        if (curr === 2) {
-            res = calcigr();
-            result.innerHTML = res;
-        }
-        if (curr === 3) {
-            res = calcflow();
-            result.innerHTML = res;
-        }
-    } else {
-        window.alert("GG");
-    }
-}
-
-function calcsertif() {
-    var count = document.getElementById("count");
-    var dostavka = document.getElementsByName("myradio");
-    var price = 0;
-    var cena = 5000;
-    var modf = 0;
-    dostavka.forEach(function (e) {
-        if (e.checked) {
-            modf = e.value;
-        }
-    });
-    price += (parseInt(cena) * parseInt(count.value) + parseInt(modf));
-
-    return price;
-}
-
-function calcigr() {
-    var count = document.getElementById("count");
-    var dostavka = document.getElementsByName("myradio");
-    var igrush = document.getElementById("select1");
-
-    var price = 0;
-    var modf;
-    dostavka.forEach(function (e) {
-        if (e.checked) {
-            modf = e.value;
-        }
-    });
-
-    price += parseInt(igrush.options[igrush.selectedIndex].value);
-    price = parseInt(count.value) * price;
-    price += parseInt(modf);
-
-    return price;
-}
-
-function calcflow() {
-    var count = document.getElementById("count");
-    var dostavka = document.getElementsByName("myradio");
-    var o1 = document.getElementsByName("props");
-    var price = 0;
-    var cena = 3000;
-    var modf = 0;
-    dostavka.forEach(function (e) {
-        if (e.checked) {
-            modf = parseInt(e.value);
-        }
-    });
-    o1.forEach(function (e) {
-        if (e.checked) {
-            modf += parseInt(e.value);
-        }
-    });
-    price += ((cena * parseInt(count.value)) + modf);
-    return price;
+function calculatePrice() {
+  var quantity = parseInt(document.getElementById("quantity").value);
+  var serviceType = document.querySelector('input[name="serviceType"]:checked').value;
+  var option = document.getElementById("option").value;
+  var property = document.getElementById("property").checked;
+  var price = 0;
+  if (serviceType === "type1") {
+    price = 2000 * quantity;
+  } else if (serviceType === "type2") {
+    price = 1500 * quantity;
+    price += option === "option0" ? 0 : 0;
+    price += option === "option1" ? 30 : 0;
+    price += option === "option2" ? 100 : 0;
+    price += option === "option3" ? 500 : 0;
+  } else if (serviceType === "type3") {
+    price = 20 * quantity;
+    price += property ? 100 : 0;
+  }
+  else if (serviceType === "type4") {
+    price = 1520 * quantity;
+    price += property ? 100 : 0;
+    price += option === "option1" ? 30 : 0;
+    price += option === "option2" ? 100 : 0;
+    price += option === "option3" ? 500 : 0;
+  } else if (serviceType === "type5") {
+    price = 3520 * quantity+quantity*25;
+    price += option === "option1" ? 30 : 0;
+    price += option === "option2" ? 100 : 0;
+    price += option === "option3" ? 500 : 0;
+    price += property ? 100 : 0;
+  }
+  document.getElementById("price").innerHTML = "Цена: " + price + " руб.";
 }
