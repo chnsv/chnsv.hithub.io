46cdefcfff2b394f0a1e031e42e16eec070e34e6
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2024-06-07 10:09:13 +0300

    Delete site directory

--- site/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('accept').addEventListener('click', function() {
-    // Перенаправляем на страницу с деталами свидания
-    window.location.href = 'details.html';
-});
-
-document.getElementById('decline').addEventListener('click', function() {
-    // Перенаправляем на страницу с сообщением "Попробовать снова?"
-    window.location.href = 'decline.html';
-});
